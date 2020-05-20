16219472cd64eecdb687b40dc0d2f7f46f0c9224
Andre Melo <andre.fabian.melo@gmail.com>
2020-05-20 20:45:27 -0300

    Redesigned projects architecture

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,14 +3,18 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
-import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import cors from 'cors';
 
-import './database';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
