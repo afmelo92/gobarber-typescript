74dd3f4a03664f893d2ef42aed9c648baa06a414
Andre Melo <andre.fabian.melo@gmail.com>
2020-06-09 23:18:08 -0300

    Add Amazon SES forgot password feature

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'andre@afmelo.com',
+      name: 'Andre da GoBarber',
+    },
+  },
+} as IMailConfig;
